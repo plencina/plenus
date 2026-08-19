3a9383b683c51fc6c75aa76ecfa608f86019800c
plencina <patolencina5151@gmail.com>
2026-08-19 02:35:10 -0300

    message from real-time update test

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,6 +1,12 @@
+import { GoAlertFill } from "react-icons/go";
+
 export default function Footer() {
     return <footer className="footer">
         <h3>Plenus</h3>
         <p>Desarrollo web</p>
+        <span className="page-status">
+            <GoAlertFill/>
+            <p>Página en construcción, contacto por mensaje privado</p>
+        </span>
     </footer>
 } 
