8797eda3f3384452429bd1b8fc1e715bfee944a8
plencina <patolencina5151@gmail.com>
2026-08-23 08:44:17 -0300

    experience

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -2,11 +2,23 @@ import { GoAlertFill } from "react-icons/go";
 
 export default function Footer() {
     return <footer className="footer">
-        <h3>Plenus</h3>
-        <p>Desarrollo web</p>
+        <p>Decepcionado por la falsedad que corrompe al hombre</p>
+        <p>Decepcionado por su miserable aporte</p>
+        <p>Decepcionado por la mujer entera</p>
+        <p>Muero en decepción</p>
+        <p>Con el honor de aprender en vida</p>
+        <p>Lo que es el repudio</p>
+        <p>Con la tristeza de haber vivido</p>
+        <p>Lo que es la traición</p>
+        <p>La deslealtad</p>
+        <p>La humillación</p>
+        <p>El fracaso</p>
+    </footer>
+}
+/*
+        <h3>Plenus</h3
         <span className="page-status">
-            <GoAlertFill/>
+            <GoAlertFill />
             <p>Página en construcción, contacto por mensaje privado</p>
         </span>
-    </footer>
-} 
+*/
